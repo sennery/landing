695261a41377451516f489d4d82d133084b8374a
likhachev-ara <mac452ares@gmail.com>
2021-12-04 01:33:53 +0500

    Header with navigation

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,13 +1,42 @@
 <template>
-    <header></header>
+    <header>
+        <template
+            v-for="link in links"
+        >
+            <NavigationLInk 
+                :name="link"
+                :key="link"
+            />
+        </template>
+    </header>
 </template>
 
 <script>
 export default {
-
+    data() {
+        return {
+            links: [
+                'works',
+                'about',
+                'contacts'
+            ]
+        }
+    }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+header {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
 
+    width: 100vw;
+    height: 10vh;
+
+    position: absolute;
+    top: 0;
+    left: 0;
+}
 </style>
