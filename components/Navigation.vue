2f1474b72db9f3732f8d6a42bef8a7ead6d347ab
likhachev-ara <mac452ares@gmail.com>
2021-12-04 20:49:19 +0500

    Added SVGs for loader and navigation

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,5 +1,14 @@
 <template>
     <header>
+        <NuxtLink 
+            to="/" 
+            class="link"
+        >
+            <SLetter 
+                class="home-icon" 
+            />
+        </NuxtLink>        
+        
         <template
             v-for="link in links"
         >
@@ -12,6 +21,8 @@
 </template>
 
 <script>
+import SLetter from '@/assets/images/letter.svg';
+
 export default {
     data() {
         return {
@@ -21,11 +32,16 @@ export default {
                 'contacts'
             ]
         }
+    },
+    components: {
+        SLetter
     }
 }
 </script>
 
 <style lang="scss" scoped>
+@import "@/assets/css/theme.scss";
+
 header {
     display: flex;
     flex-direction: row;
@@ -33,10 +49,33 @@ header {
     align-items: center;
 
     width: 100vw;
-    height: 10vh;
+    height: 15vh;
 
     position: absolute;
     top: 0;
     left: 0;
+
+    .link {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        
+        height: 100%;
+        margin: 0.75em;
+    }
+
+    .home-icon {
+        height: 25%;
+        padding: 0.75em;
+
+        transition: 1s $easing-primary;
+        transform: rotateZ(-90deg);
+
+        cursor: pointer; 
+
+        &:hover {
+           transform: rotateZ(-90deg) scale(1.1); 
+        }
+    }
 }
 </style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,43 +1,52 @@
 export default {
-  // Global page headers: https://go.nuxtjs.dev/config-head
-  head: {
-    title: 'landing',
-    htmlAttrs: {
-      lang: 'en'
+    // Global page headers: https://go.nuxtjs.dev/config-head
+    head: {
+        title: 'landing',
+        htmlAttrs: {
+            lang: 'en'
+        },
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            { hid: 'description', name: 'description', content: '' },
+            { name: 'format-detection', content: 'telephone=no' }
+        ],
+        link: [
+            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+        ]
     },
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+
+    // Global CSS: https://go.nuxtjs.dev/config-css
+    css: [
+        '@/assets/css/reset.css',
+        '@/assets/css/inject.scss'
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-      '@/assets/css/reset.css',
-      '@/assets/css/inject.scss'
-  ],
+    // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+    plugins: [
+    ],
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+    // Auto import components: https://go.nuxtjs.dev/config-components
+    components: true,
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
+    // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+    buildModules: [
+        'nuxt-svg-loader',
+    ],
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-  ],
+    svgLoader: {
+        svgoConfig: {
+            plugins: [
+                { prefixIds: false } // Disables prefixing for SVG IDs
+            ]
+        }
+    },
 
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-  ],
+    // Modules: https://go.nuxtjs.dev/config-modules
+    modules: [
+    ],
 
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
+    // Build Configuration: https://go.nuxtjs.dev/config-build
+    build: {
+    }
 }
