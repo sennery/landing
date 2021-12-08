498dae634b43f65e2f95eade2de1443cd2b29b8b
likhachev-ara <mac452ares@gmail.com>
2021-12-08 20:52:22 +0500

    [contacts] Added contacts link

--- pages/contacts.vue
+++ pages/contacts.vue
@@ -13,6 +13,18 @@
                     contacts
                 </span>
 
+                <template
+                    v-for="contact in contacts"
+
+                >
+                    <ContactLink
+                        class="contact"
+                        :key="contact.name"
+                        :name="contact.name"
+                        :link="contact.link"
+                        :color="contact.color"
+                    />
+                </template>
             </div>
         </Container>        
     </main>
@@ -20,7 +32,32 @@
 
 <script>
 export default {
-
+    data() {
+        return {
+            contacts: [
+                {
+                    name: 'GitHub',
+                    color: '#bb268a',
+                    link: ''
+                },
+                {
+                    name: 'LinkedIn',
+                    color: '#9b2a9f',
+                    link: ''
+                },
+                {
+                    name: 'Telegram',
+                    color: '#7132bb',
+                    link: ''
+                },
+                {
+                    name: 'Email',
+                    color: '#5a38cb',
+                    link: ''
+                }
+            ]
+        }
+    }
 }
 </script>
 
@@ -30,17 +67,26 @@ export default {
 .side {
     display: flex;
     flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
     background-color: $color-background;
 
     position: absolute;
     top: 0;
     left: 50%;
 
-    height: 100vh;
+    height: 100vh;    
 
     .title {
         font-size: 10em;
-        transform: translateX(-10%);
+        transform: translate(-10%, -20%);
+
+    }
+
+    .contact {
+        width: 50%;
+        font-size: 2.5em;
+        margin: 0.5em 0 0 1em;
     }
 }
 </style>
