ca3c2e159d9e50ee57bae58ff532bebbfa874766
Arsenii Likhachev <mac452ares@gmail.com>
2022-12-30 00:07:16 +0700

    feat: add background component

---
+++ src/components/BackgroundScene.vue
@@ -0,0 +1,30 @@
+<template>
+  <aside>
+    <img       
+      src="@/assets/letter.svg" 
+      alt="Vue logo" 
+      class="logo" 
+      width="125" 
+      height="125"
+    >
+  </aside>
+</template>
+
+<style scoped>
+aside {
+  display: flex;
+  justify-content: flex-end;
+  position: fixed;
+  left: 0;
+  top: 0;
+  height: 100vh;
+  width: 100vw;
+  overflow: hidden;
+}
+
+.logo {  
+  z-index: -999;
+  transform: rotateZ(90deg) scale(10) translate(-1rem, 1rem);
+  transform-origin: 0 50%;
+}
+</style>
