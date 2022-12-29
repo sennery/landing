02f58ef3fcbd809c9bcc39d1f9c2c6c67ef121c8
Arsenii Likhachev <mac452ares@gmail.com>
2022-12-30 00:06:56 +0700

    feat: add title page

---
+++ src/components/PageTitle.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <section class="section-title">
+    <h1>Arsenii Likhachev</h1>     
+    <h2>web developer</h2>
+  </section>
+</template>
+
+<style scoped>
+.section-title {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: 100vh;
+}
+</style>
