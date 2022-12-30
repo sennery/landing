9baf5f0c1a8033f6ade0ac8bedeacbb49b86be8a
Arsenii Likhachev <mac452ares@gmail.com>
2022-12-30 23:56:00 +0700

    feat: create works section

---
+++ src/components/SectionWorks.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import PageWorksItem from '@/components/SectionWorksItem.vue'
+
+const WORKS = [
+    {
+        title: 'Frontend architect',
+        years: '2022-nowadays',
+        description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat sunt porro ad, facilis voluptas assumenda quidem rem aliquam tenetur in vitae nihil explicabo itaque quaerat tempore soluta cumque a sequi.'
+    },
+    {
+        title: 'Fullstack developer',
+        years: '2020-2022',
+        description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat sunt porro ad, facilis voluptas assumenda quidem rem aliquam tenetur in vitae nihil explicabo itaque quaerat tempore soluta cumque a sequi.'
+    }
+]
+</script>
+
+<template>
+  <section class="section-works">
+    <PageWorksItem 
+      v-for="(work, index) in WORKS"
+      :key="index"
+      :title="work.title"
+      :years="work.years"
+      :description="work.description"
+    />
+  </section>
+</template>
+
+<style scoped>
+.section-works {
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+  gap: 10rem;
+  margin: 10rem 0;
+}
+</style>
---
+++ src/components/SectionWorksItem.vue
@@ -0,0 +1,70 @@
+<script setup lang="ts">
+import { ref, computed } from 'vue'
+import { vIntersectionObserver } from '@vueuse/components'
+import { animate } from 'popmotion'
+
+const animationProgress = ref(0);
+const animationTranslateParagraph = computed(() => `${animationProgress.value * 10 - 10}rem`)
+
+let animation: { stop: () => void };
+
+function onIntersectionObserver ([{ isIntersecting }] : Array<{ isIntersecting: boolean }>) {
+  animation?.stop();
+  animation = animate({
+    from: animationProgress.value,
+    to: isIntersecting ? 1 : 0,
+    type: 'spring',
+    stiffness: 100,
+    onUpdate: latest => animationProgress.value = latest
+})
+}
+
+defineProps({
+  title: { 
+    type: String,
+    required: true
+  },
+  years: { 
+    type: String,
+    required: true
+  },
+  description: { 
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <article 
+    v-intersection-observer="[onIntersectionObserver, { threshold: 0.5 }]"
+    class="article-works"
+  >
+    <div>
+      <h2>{{ title }}</h2>     
+      <h3>{{ years }}</h3>
+    </div>
+    <p>{{ description }}</p>
+  </article>
+</template>
+
+<style scoped>
+.article-works {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 50rem;
+}
+
+.article-works h2,
+.article-works h3,
+.article-works p {
+  transform-origin: 0% 50%;
+  opacity: v-bind(animationProgress);
+  transform: translateX(v-bind(animationTranslateParagraph));
+}
+
+.article-works div {
+    margin: 2rem 0;
+}
+</style>
