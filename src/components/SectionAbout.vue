5c38b6f5eb69af4fff8f0e570f79645316d5d6ee
Arsenii Likhachev <mac452ares@gmail.com>
2023-01-01 21:05:19 +0700

    feat: add passed time composables for age and exp

--- src/components/SectionAbout.vue
+++ src/components/SectionAbout.vue
@@ -1,8 +1,20 @@
 <script setup lang="ts">
-import { computed } from 'vue'
+import { ref, computed } from 'vue'
 import { vIntersectionObserver } from '@vueuse/components'
 import { useIntersectAnimation } from '@/composables/intersectAnimation';
+import { useTimePassed } from '@/composables/getTimePassed';
 
+const { years: age } = useTimePassed('1999-03-18T05:00:00+0500')
+const { years, weeks, days, hours, minutes, seconds, sumSeconds } = useTimePassed('2020-06-16T05:00:00+0500')
+
+const onlySeconds = ref(true)
+const changeExpTime = () => onlySeconds.value = !onlySeconds.value
+
+const englishPluralRules = new Intl.PluralRules('en');
+
+console.log(englishPluralRules.select(0))
+console.log(englishPluralRules.select(1))
+console.log(englishPluralRules.select(2))
 const { animationProgress, onIntersect } = useIntersectAnimation();
 
 const animationTranslateParagraph = computed(() => `${10 - animationProgress.value * 10}rem`)
@@ -16,8 +28,27 @@ const animationTranslateTitle = computed(() => `${5 - animationProgress.value * 
   >
     <h2>about me</h2>     
     <p>
-      Hi, my name is Arsenii Likhachev. <br> 
-      I'm a 23 y.o. web developer making complex and clean web applications. <br>
+      Hi, my name is Arsenii Likhachev. 
+      I'm a <span>{{ age }}</span> y.o. web developer. Making complex and clean web applications for
+      <span 
+        v-if="onlySeconds"
+        class="exp-time"
+        @click="changeExpTime"
+      >
+        {{ sumSeconds }} sec.
+      </span> 
+      <span
+        v-else
+        class="exp-time"
+        @click="changeExpTime"
+      >
+        <span>{{ `${years} ${englishPluralRules.select(years) === 'one' ? 'year' : 'years'} ` }}</span> 
+        <span>{{ `${weeks} ${englishPluralRules.select(weeks) === 'one' ? 'week' : 'weeks'} ` }}</span>
+        <span>{{ `${days} ${englishPluralRules.select(days) === 'one' ? 'day' : 'days'} ` }}</span>
+        <span>{{ `${hours} ${englishPluralRules.select(hours) === 'one' ? 'hour' : 'hours'} ` }}</span>
+        <span>{{ `${minutes} ${englishPluralRules.select(minutes) === 'one' ? 'minute' : 'minutes'} ` }}</span>
+        <span>{{ `${seconds} ${englishPluralRules.select(seconds) === 'one' ? 'second' : 'seconds'} ` }}</span>. 
+      </span>
       Love learning and developing. 
     </p>
   </section>
@@ -37,5 +68,10 @@ const animationTranslateTitle = computed(() => `${5 - animationProgress.value * 
 .section-about > h2 {
   transform: translateY(v-bind(animationTranslateTitle));
   margin: 2rem 0;
+}
+
+.exp-time {
+  cursor: pointer;
+  color: #262c7a;
 }
 </style>
---
+++ src/composables/getTimePassed.ts
@@ -0,0 +1,21 @@
+import { ref, computed } from "vue";
+
+export function useTimePassed(date: string) {
+    const stampDate = +new Date(date)
+    const stampNow = ref(+new Date())
+
+    setInterval(() => stampNow.value = +new Date(), 1000)
+
+    const diff = computed(() => stampNow.value - stampDate)
+
+    const years = computed(() => Math.floor(diff.value / (1000 * 60 * 60 * 24 * 365.25)))
+    const weeks = computed(() => Math.floor((diff.value / (1000 * 60 * 60 * 24 * 7)) - years.value * (365.25 / 7)))
+    const days = computed(() => Math.floor((diff.value / (1000 * 60 * 60 * 24))  - years.value * 365.25 - weeks.value * 7))
+    const hours = computed(() => Math.floor((diff.value / (1000 * 60 * 60)) - 24 * (years.value * 365.25 + weeks.value * 7 + days.value)))
+    const minutes = computed(() => Math.floor((diff.value / (1000 * 60) - 60 * (24 * (years.value * 365.25 + weeks.value * 7 + days.value) + hours.value))))
+    const seconds = computed(() => Math.floor(diff.value / 1000 - 60 * (60 * (24 * (years.value * 365.25 + weeks.value * 7 + days.value) + hours.value) + minutes.value)))
+
+    const sumSeconds = computed(() => Math.floor(diff.value / 1000))
+
+    return { years, weeks, days, hours, minutes, seconds, sumSeconds }
+}
