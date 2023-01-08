122b63de6c36516124b6cbf79ea6e983646c7474
Arsenii Likhachev <mac452ares@gmail.com>
2023-01-08 19:32:14 +0700

    feat: add meta tags for social media preview

--- src/App.vue
+++ src/App.vue
@@ -6,27 +6,30 @@ import SectionWorks from '@/components/SectionWorks.vue'
 import SectionContacts from '@/components/SectionContacts.vue'
 import { useHead } from '@vueuse/head'
 
+const description = 'Hi, my name is Arsenii Likhachev. I am a web developer and I make complex and clean web applications. Love to develop and learn new things.' 
+
 useHead({
   title: 'Arsenii Likhachev',
   link: [
-    {
-      rel: 'icon',
-      sizes: 'any',
-      href: '/favicon.ico'
-    },
-    {
-      rel: 'icon',
-      type: 'image/svg+xml',
-      href: '/icon.svg'
-    },
-    {
-      rel: 'apple-touch-icon',
-      href: '/apple-touch-icon.png'
-    },
-    {
-      rel: 'manifest',
-      href: '/manifest.webmanifest'
-    }
+    { rel: 'icon', sizes: 'any', href: '/favicon.ico' },
+    { rel: 'icon', type: 'image/svg+xml', href: '/icon.svg' },
+    { rel: 'apple-touch-icon', href: '/apple-touch-icon.png' },
+    { rel: 'manifest', href: '/manifest.webmanifest' }
+  ],
+  meta: [
+    { name: 'description', content: description },
+    { name: 'author', content: 'sennery' },
+    { property: 'og:title', content: 'Arsenii Likhachev' },
+    { property: 'og:description', content: description },
+    { property: 'og:type', content: 'website' },
+    { property: 'og:url', content: 'https://sennery.dev' },
+    { property: 'og:image', content: 'https://sennery.dev/share-image.png' },
+    { name: 'twitter:card', content: 'summary_large_image' },
+    { name: 'twitter:title', content: 'Arsenii Likhachev' },
+    { name: 'twitter:description', content: description },
+    { name: 'twitter:domain', content: 'sennery.dev' },
+    { name: 'twitter:url', content: 'https://sennery.dev' },
+    { name: 'twitter:image', content: 'https://sennery.dev/share-image.png' }
   ]
 })
 </script>
