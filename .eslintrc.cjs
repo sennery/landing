16e02638003344261d32bb23e376c515f0f328ef
Arsenii Likhachev <mac452ares@gmail.com>
2023-01-10 23:13:47 +0700

    chore: add my personal eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,20 +2,8 @@
 require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
-  root: true,
-  'extends': [
-    'plugin:vue/vue3-recommended',
-    'eslint:recommended',
-    '@vue/eslint-config-typescript'
+  extends: [
+    '@sennery',
+    '@vue/eslint-config-typescript',
   ],
-  parserOptions: {
-    ecmaVersion: 'latest'
-  },
-  rules: {
-    semi: ['error', 'never'],
-    indent: ['error', 2],
-    'vue/script-indent': ['error', 2],
-    'vue/html-indent': ['error', 2],
-    'vue/component-name-in-template-casing': ['error', 'PascalCase']
-  }
 }
