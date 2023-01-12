c8c45e28c4ad3835cdeb637a34c3a2c9dbd49a93
Arsenii Likhachev <mac452ares@gmail.com>
2023-01-13 01:07:00 +0700

    chore: update dependencies

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,5 @@
-/* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
-
 module.exports = {
   extends: [
     '@sennery',
-    '@vue/eslint-config-typescript',
   ],
 }
