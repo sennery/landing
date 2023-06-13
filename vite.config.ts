19d122a310a020eca4965f9d95bf9318a3db1507
Arsenii Likhachev <mac452ares@gmail.com>
2023-06-13 08:19:34 +0500

    chore: move vite env to src

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,8 @@
-/// <reference types="vite-ssg" />
-
 import { URL, fileURLToPath } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
